498a954de7e3d4186a89a9b882bd766c720ab909
SunilVerma0358 <sunilverma93060@gmail.com>
2024-04-18 16:42:32 +0530

    update

--- src/components/MyWorks.jsx
+++ src/components/MyWorks.jsx
@@ -7,7 +7,7 @@ const MyWorks = () => {
     setfirst(Tab);
   }
   return (
-    <div className="bg-black   vh-100">
+    <div className="bg-black ">
       <div className="container py-5 ">
         <div className="flex items-center justify-between">
           {" "}
